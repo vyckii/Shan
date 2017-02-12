589af1bcd2937326db96c0296426e74181dd7576
stephaniefei <sfei@wharton.upenn.edu>
2017-02-12 17:25:47 -0500

    basic welcome screen

---
+++ app/src/main/java/cis350/upenn/edu/remindmelater/MainActivity.java
@@ -0,0 +1,18 @@
+package cis350.upenn.edu.remindmelater;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by stephaniefei on 2/12/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
