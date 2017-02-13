f2bde36c947c9b282853bb4e6471b86e9f9e545d
stephaniefei <sfei@wharton.upenn.edu>
2017-02-12 22:55:54 -0500

    updated welcome screen

--- app/src/main/java/cis350/upenn/edu/remindmelater/MainActivity.java
+++ app/src/main/java/cis350/upenn/edu/remindmelater/MainActivity.java
@@ -3,6 +3,8 @@ package cis350.upenn.edu.remindmelater;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by stephaniefei on 2/12/17.
@@ -10,9 +12,34 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button createAcctButton;
+    Button logInButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createAcctButton = (Button) findViewById(R.id.createAcct);
+        createAcctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //trigger create account screen
+                System.out.println("create");
+            }
+        });
+        logInButton = (Button) findViewById(R.id.logIn);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //trigger create login screen
+                System.out.println("login");
+            }
+        });
     }
+
+//    public void onButtonPress(View v) {
+//        System.out.println("hi");
+//    }
+
+
 }
